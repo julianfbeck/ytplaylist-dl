2b7da8b0086ac6996cbee9adb9470c52f7dbcfbf
Julian beck <ju-fa-beck@t-online.de>
2018-07-06 10:21:54 +0900

    fixed tests

--- test.js
+++ test.js
@@ -12,11 +12,21 @@ describe("ytplaylist-dl", async function () {
     rimraf.sync("testfolder");
     fs.mkdirSync("testfolder");
 
-    it("Downloading single file", async function () {
+    it("Downloading playlist", async function () {
         let videos = await m("https://www.youtube.com/playlist?list=PLfpHPxe91z9NEwLMsxfmAehlZnoTzRFB8", "testfolder");
         expect(videos).to.be.an("array");
         let result = fs.readdirSync("testfolder");
         expect(result[0]).to.be.equal("Feelin29 Feat.KOJOE - 5lack.mp4")
+        rimraf.sync("testfolder");
+        fs.mkdirSync("testfolder");
     });
-    rimraf.sync("testfolder");
+
+
+    it("Downloading playlist with selecteded video", async function () {
+        let videos = await m("https://www.youtube.com/watch?v=pRk9K1eB-JQ&list=PLfpHPxe91z9NEwLMsxfmAehlZnoTzRFB8&index=1", "testfolder");
+        expect(videos).to.be.an("array");
+        let result = fs.readdirSync("testfolder");
+        expect(result[0]).to.be.equal("Feelin29 Feat.KOJOE - 5lack.mp4")
+        rimraf.sync("testfolder");
+    });
 });
