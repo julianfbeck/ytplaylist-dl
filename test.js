81ef1dd03f66677f3185bbc94cbe2cf639ff8a33
Julian beck <ju-fa-beck@t-online.de>
2018-07-06 10:46:15 +0900

    test for single urls

--- test.js
+++ test.js
@@ -21,6 +21,15 @@ describe("ytplaylist-dl", async function () {
         fs.mkdirSync("testfolder");
     });
 
+    it("Download without playlist", async function () {
+        let videos = await m("https://www.youtube.com/watch?v=pRk9K1eB-JQ&", "testfolder");
+        expect(videos).to.be.an("array");
+        let result = fs.readdirSync("testfolder");
+        expect(result[0]).to.be.equal("Feelin29 Feat.KOJOE - 5lack.mp4")
+        rimraf.sync("testfolder");
+        fs.mkdirSync("testfolder");
+    });
+
 
     it("Downloading playlist with selecteded video", async function () {
         let videos = await m("https://www.youtube.com/watch?v=pRk9K1eB-JQ&list=PLfpHPxe91z9NEwLMsxfmAehlZnoTzRFB8&index=1", "testfolder");
